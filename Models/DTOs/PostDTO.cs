eb588c3e3b95fdcdeb9e8c172976a72194b399b3
Austin <cookaustin46@gmail.com>
2026-07-08 15:30:40 -0500

    Added 2 posts endpoints

--- Models/DTOs/PostDTO.cs
+++ Models/DTOs/PostDTO.cs
@@ -6,6 +6,7 @@ public class PostDTO
     public int Id { get; set; }
     public string Title { get; set; }
     public string Image { get; set; }
+    public string Body { get; set; }
     public DateTime PubDate { get; set; }
     public int UserId { get; set; }
     public int CategoryId { get; set; }
