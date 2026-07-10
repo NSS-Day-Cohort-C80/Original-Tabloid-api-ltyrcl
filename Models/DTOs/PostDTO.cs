c0e0edc2fda84d77475ca3085a6eaa33a6f79f66
Austin <cookaustin46@gmail.com>
2026-07-10 11:50:27 -0500

    added post body to DTO

--- Models/DTOs/PostDTO.cs
+++ Models/DTOs/PostDTO.cs
@@ -21,6 +21,7 @@ public class CreatePostDTO
 {
     public string Title { get; set; }
     public string Image { get; set; }
+    public string Body { get; set; }
     public DateTime PubDate { get; set; }
     public int UserId { get; set; }
     public int CategoryId { get; set; }
